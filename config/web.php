0449b47e0745797397695fe0324acfa5fe36555f
Tomas B. Cabagay Jr <tomas.cabagay@gmail.com>
2015-09-12 23:28:21 +0800

    Update web params - add urlManager

--- config/web.php
+++ config/web.php
@@ -38,6 +38,12 @@ $config = [
             ],
         ],
         'db' => require(__DIR__ . '/db.php'),
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'enableStrictParsing' => false,
+            'suffix' => '.html',
+        ],
     ],
     'params' => $params,
 ];
